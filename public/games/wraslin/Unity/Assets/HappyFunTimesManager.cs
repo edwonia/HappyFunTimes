e6376a6dd29acb8b74f4b751de2a57da0bc95f8c
Steven Edwon <edwon@me.com>
2014-06-05 18:17:52 +0900

    the beginnings of this magical wraslin game

---
+++ public/games/wraslin/Unity/Assets/HappyFunTimesManager.cs
@@ -0,0 +1,48 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//using HappyFunTimes;
+//
+//public class HappyFunTimesManager : MonoBehaviour {
+//
+//	GameServer.Options options;
+//	GameServer m_server;
+//	
+//
+//	void Start () 
+//	{
+//		GameServer.Options options = new GameServer.Options();
+//		options.gameId = "simple";
+//		m_server = new GameServer(options, gameObject);
+//		m_server.Init();
+//
+//		StartEvents();
+//
+//	}
+//
+//	void StartEvents ()
+//	{
+//		m_server.OnPlayerConnect += StartNewPlayer;
+////		m_server.OnConnect += Connected;
+////		m_server.OnDisconnect += Remove;
+//	}
+//
+////	private void Remove(object sender, EventArgs e) {
+////		Destroy(gameObject);
+////	}
+//	
+//
+//	void StartNewPlayer (object sender, PlayerConnectMessageArgs e)
+//	{
+//		// Spawn a new player then add a script to it.
+//		Vector3 pos = new Vector3(0,0,0);
+//		GameObject gameObject = (GameObject)Instantiate(Resources.Load("player"), pos, Quaternion.identity);
+//
+//	}
+//	
+//
+//	void Update () 
+//	{
+//	
+//	}
+//
+//}
